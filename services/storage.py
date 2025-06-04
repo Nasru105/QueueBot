818a1074371fee9e0fa86e08487910b8de0ce5a8
Nasru <nasibov2005@yandex.ru>
2025-06-04 18:50:37 +0300

    оптимизация

--- services/storage.py
+++ services/storage.py
@@ -16,25 +16,4 @@ def save_data(queues, last_queue_message):
     with open(FILE, "w", encoding="utf-8") as f:
         json.dump({"queues": queues, "last_queue_message": last_queue_message}, f)
 
-## Функция для загрузки данных из файла
-#def load_data():
-#    # Если файл не существует, возвращаем пустые словари
-#    if not os.path.exists(FILE):
-#        return {}, {}
-#
-#    # Открываем файл и загружаем содержимое
-#    with open(FILE, "r", encoding="utf-8") as f:
-#        data = json.load(f)
-#        # Возвращаем данные из файла: очередь и последние ID сообщений
-#        return data.get("queues", {}), data.get("last_queue_message", {})
-#
-#
-## Функция для сохранения данных в файл
-#def save_data(queues, last_queue_message):
-#    # Открываем файл на запись и сохраняем данные в формате JSON
-#    with open(FILE, "w", encoding="utf-8") as f:
-#        json.dump({
-#            "queues": queues,
-#            "last_queue_message": last_queue_message
-#        }, f, ensure_ascii=False,
-#            indent=2)  # ensure_ascii=False позволяет сохранить кириллицу, indent=2 — форматирование
+
